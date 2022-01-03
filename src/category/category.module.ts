d3c17c9df0bc5c8fc61aa9ca0af348b4d09237cc
Ayub Abdullah <Ayubgardy898@gmail.com>
2022-01-03 22:17:39 +0300

    add styles and favicon

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,4 +1,4 @@
-import {  Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,7 +6,7 @@ import { Category } from './entities/category.entity';
 import { LogsModule } from 'src/logging/logging.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category]),LogsModule],
+  imports: [TypeOrmModule.forFeature([Category]), LogsModule],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
